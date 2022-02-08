e573dcf085c66b00b043099d2098f16200dd801b
Klemense <klemenseloh@gmail.com>
2022-02-08 15:29:51 -0500

    updated

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-const dodger = document.getElementById("dodger"); 
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+})
 
 function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
-    }
-  } 
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
 
-  document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-  }); 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
 
-  function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left < 360) {
-      dodger.style.left = `${left + 1}px`;
-    }
-  } 
+function moveDodgerRight(){
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
 
-  document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowRight") {
-      moveDodgerRight();
-    }
-  }); 
+  if (left < 360){
+    dodger.style.left = `${left +1}px`
+  }
+}
